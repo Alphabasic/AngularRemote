79e2f7ab7752776b4bd3470e0b4db08398866b3e
Alphabasic <Samuel.a.hardy@gmail.com>
2017-03-19 20:55:13 -0400

    refactor getAllPosts to promise

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
-import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/toPromise';
+
+import { Post } from './post';
 
 @Injectable()
 export class PostsService {
@@ -8,8 +10,19 @@ export class PostsService {
   constructor(private http: Http) { }
 
   // Get all posts from the API
-  getAllPosts() {
-    return this.http.get('/api/posts')
-      .map(res => res.json());
+  getAllPosts(): Promise<Post[]> {
+    return this.http.get('http://localhost:3000/api/posts')
+      .toPromise()
+      .then( res => res.json().data as Post[])
+      .catch(this.handleError)
+  }
+
+  updatePost(id: string) {
+    
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.log('An error occurred', error);
+    return Promise.reject(error.message || error);
   }
 }
