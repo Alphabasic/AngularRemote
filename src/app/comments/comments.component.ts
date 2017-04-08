5ff626310781952bda3c8f696faadaaa0a995630
Alphabasic <Samuel.a.hardy@gmail.com>
2017-04-08 19:34:29 -0400

    mediate inputs

--- src/app/comments/comments.component.ts
+++ src/app/comments/comments.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { CommentsService } from '../comments.service'
 
 @Component({
   selector: 'app-comments',
--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { PostsService } from '../posts.service';
