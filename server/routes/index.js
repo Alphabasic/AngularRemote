4954d9747527512719ae0bdd8c3b13f33bf02a04
Alphabasic <Samuel.a.hardy@gmail.com>
2017-04-11 21:31:03 -0400

    fix base path for api

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,6 +3,12 @@ const routes = requireDirectory(module);
 const express = require('express');
 
 module.exports = (app) => {
+    // base
+    app.get('/api/', (req, res) => {
+        res.status(200).send('Success');
+    });
+
+    //routes
     Object.keys(routes).forEach(routeName => {
         let router = express.Router();
         require('./' + routeName)(router);
